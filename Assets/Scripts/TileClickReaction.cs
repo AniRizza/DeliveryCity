1714016a8f34dad66e9782b2d5de537b9eec784e
AniRizza <niksofi29@gmail.com>
2023-07-14 22:45:10 +0300

    highlighting car track

--- Assets/Scripts/TileClickReaction.cs
+++ Assets/Scripts/TileClickReaction.cs
@@ -5,11 +5,13 @@ using UnityEngine.AI;
 
 public class TileClickReaction : MonoBehaviour
 {
-
+    [SerializeField]
+    private bool isTileOnPath;
+    public GameObject blobPrefab;
     // Start is called before the first frame update
     void Start()
     {
-
+        isTileOnPath = false;
     }
 
     // Update is called once per frame
@@ -20,10 +22,18 @@ public class TileClickReaction : MonoBehaviour
             int y = GetComponent<TileCharacteristic>().GetY();
 
             Debug.Log("Pressed left-click on x " + x + " y " + y);
-            GameObject.Find("Car").GetComponent<NavMeshAgent>().SetDestination(transform.position);
+            GameObject.Find("Car").GetComponent<CarMovementPath>().SetTarget(transform);
+            //navMeshAgent.SetDestination(transform.position);
         }
         if (Input.GetMouseButtonDown(1)) {
             Debug.Log("Pressed right-click on x " + GetComponent<TileCharacteristic>().GetX() + " y " + GetComponent<TileCharacteristic>().GetY());
+        }
+    }
+
+    public void AddTileToPath() {
+        if (!isTileOnPath) {
+            isTileOnPath = true;
+            Instantiate(blobPrefab, new Vector3(transform.position.x, 0.4f, transform.position.z), blobPrefab.transform.rotation);
         }
     }
 }
