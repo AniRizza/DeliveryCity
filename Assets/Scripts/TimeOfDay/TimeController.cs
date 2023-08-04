06b89770493bdf8c1e4fa30c06c5ed411560a1cc
AniRizza <niksofi29@gmail.com>
2023-08-04 17:03:39 +0300

    updated time controller

---
+++ Assets/Scripts/TimeOfDay/TimeController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField]
+    private float startHour;
+    [SerializeField]
+    private float endHour;
+    [SerializeField]
+    private Light sunLightSource;
+    [SerializeField]
+    private GameObject timelinIndicator;
+    [SerializeField]
+    private float sunriseHour = 7;
+    [SerializeField]
+    private float sunsetHour = 19;
+    private TimeSpan sunriseTime;
+    private TimeSpan sunsetTime;
+    private TimeSpan startTime;
+    private TimeSpan endTime;
+    private TimeSpan shiftLength;
+    private DateTime currentTime;
+    private Vector3 timelineIndicatorStartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+        sunriseTime = TimeSpan.FromHours(sunriseHour);
+        sunsetTime = TimeSpan.FromHours(sunsetHour);
+        startTime = TimeSpan.FromHours(startHour);
+        endTime = TimeSpan.FromHours(endHour);
+        shiftLength = CalculateTimeDifference(startTime, endTime);
+        timelineIndicatorStartPosition = timelinIndicator.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimeOfDay();
+        RotateSun();
+        PlaceTimelineMarker();
+    }
+
+    private void RotateSun() {
+        float sunLightRotation;
+        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime) {
+            TimeSpan sunriseToSunsetDuration = CalculateTimeDifference(sunriseTime, sunsetTime);
+            TimeSpan timeSinceSunrise = CalculateTimeDifference(sunriseTime, currentTime.TimeOfDay);
+            double percentage = timeSinceSunrise.TotalMinutes / sunriseToSunsetDuration.TotalMinutes;
+            sunLightRotation = Mathf.Lerp(0, 180, (float) percentage);
+        }
+        else {
+            TimeSpan sunsetToSunriseDuration = CalculateTimeDifference(sunsetTime, sunriseTime);
+            TimeSpan timeSinceSunset = CalculateTimeDifference(sunsetTime, currentTime.TimeOfDay);
+            double percentage = timeSinceSunset.TotalMinutes / sunsetToSunriseDuration.TotalMinutes;
+            sunLightRotation = Mathf.Lerp(180, 360, (float) percentage);
+        }
+        sunLightSource.transform.rotation = Quaternion.Euler(sunLightRotation, 90f, 0f);
+    }
+
+    private void PlaceTimelineMarker() {
+        float currentTimelinIndicatorPosition;
+        if (currentTime.TimeOfDay < endTime) {
+            TimeSpan timeSinceShiftStart = CalculateTimeDifference(startTime, currentTime.TimeOfDay);
+            double percentage = timeSinceShiftStart.TotalMinutes / shiftLength.TotalMinutes;
+            currentTimelinIndicatorPosition = Mathf.Lerp(timelineIndicatorStartPosition.x, (float) Screen.width - timelineIndicatorStartPosition.x, (float) percentage);
+        }
+        else {
+            currentTimelinIndicatorPosition = (float) Screen.width - timelineIndicatorStartPosition.x;
+        }
+        timelinIndicator.transform.position = new Vector3(currentTimelinIndicatorPosition, timelineIndicatorStartPosition.y, timelineIndicatorStartPosition.z);
+    }
+
+    private TimeSpan CalculateTimeDifference (TimeSpan fromTime, TimeSpan toTime) {
+        TimeSpan difference = toTime - fromTime;
+        if (difference. TotalSeconds < 0) {
+            difference += TimeSpan. FromHours(24);
+        }
+        return difference;
+    }
+
+    private void UpdateTimeOfDay() {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * 1000);
+    }
+}
